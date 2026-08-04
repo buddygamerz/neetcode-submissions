7585a22f6bc77598023999d4c0e01a57932ae661
Rajvardhan Singh <singhrajvardhan203@gmail.com>
2026-08-04 15:28:57 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+         for(int i=0;i<stones.length;i++){
+            maxHeap.offer(stones[i]);
+         }
+       
+        while(maxHeap.size()>1){
+            int first = maxHeap.poll();   // largest
+            int second = maxHeap.poll();
+            if(first-second!=0){
+               maxHeap.offer(first-second);
+                
+            }
+            
+           
+
+        }
+        
+        if(maxHeap.peek()!=null){
+        return maxHeap.poll();
+    }
+    return 0;
+}}
